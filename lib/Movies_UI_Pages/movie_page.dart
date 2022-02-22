a16842d13596343780197129313851320bcde5aa
AmanMobDev <aman232mishra@gmail.com>
2022-02-23 00:04:20 +0530

    update

--- lib/Movies_UI_Pages/movie_page.dart
+++ lib/Movies_UI_Pages/movie_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MoviePage extends StatefulWidget {
@@ -196,7 +198,11 @@ class _MoviePageState extends State<MoviePage> {
                                             MainAxisAlignment.spaceBetween,
                                         children: [
                                           GestureDetector(
-                                            onTap: () {},
+                                            onTap: () {
+                                              setState(() {
+                                                showDialogBox();
+                                              });
+                                            },
                                             child: const Icon(
                                               Icons.download_rounded,
                                               size: 20,
@@ -286,167 +292,35 @@ class _MoviePageState extends State<MoviePage> {
                   ],
                 ),
               ),
-              // Row(
-              //   crossAxisAlignment: CrossAxisAlignment.start,
-              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //   children: [
-              //     Expanded(
-              //       child: Padding(
-              //         padding: const EdgeInsets.symmetric(
-              //             vertical: 15.0, horizontal: 10.0),
-              //         child: Column(
-              //           crossAxisAlignment: CrossAxisAlignment.start,
-              //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //           mainAxisSize: MainAxisSize.min,
-              //           children: const [
-              //             Text(
-              //               'SPIDER_MAN',
-              //               style: TextStyle(
-              //                 fontWeight: FontWeight.bold,
-              //                 fontSize: 22,
-              //                 color: Colors.white,
-              //               ),
-              //             ),
-              //             Padding(
-              //               padding: EdgeInsets.symmetric(vertical: 8.0),
-              //               child: Text(
-              //                 'NO WAY HOME',
-              //                 style: TextStyle(
-              //                   fontWeight: FontWeight.bold,
-              //                   fontSize: 12,
-              //                   color: Colors.grey,
-              //                 ),
-              //               ),
-              //             ),
-              //           ],
-              //         ),
-              //       ),
-              //     ),
-              //     Expanded(
-              //       child: Row(
-              //         crossAxisAlignment: CrossAxisAlignment.start,
-              //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //         children: [
-              //           Padding(
-              //             padding: const EdgeInsets.symmetric(vertical: 10.0),
-              //             child: Column(
-              //               mainAxisSize: MainAxisSize.min,
-              //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //               children: [
-              //                 GestureDetector(
-              //                   onTap: () {},
-              //                   child: const Icon(
-              //                     Icons.add_rounded,
-              //                     size: 20,
-              //                     color: Colors.white,
-              //                   ),
-              //                 ),
-              //                 const Padding(
-              //                   padding: EdgeInsets.symmetric(vertical: 10.0),
-              //                   child: Text(
-              //                     'WATCHLIST',
-              //                     style: TextStyle(
-              //                       fontWeight: FontWeight.w500,
-              //                       fontSize: 12,
-              //                       color: Colors.white,
-              //                     ),
-              //                   ),
-              //                 ),
-              //               ],
-              //             ),
-              //           ),
-              //           Padding(
-              //             padding: const EdgeInsets.symmetric(vertical: 10.0),
-              //             child: Column(
-              //               mainAxisSize: MainAxisSize.min,
-              //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //               children: [
-              //                 GestureDetector(
-              //                   onTap: () {},
-              //                   child: const Icon(
-              //                     Icons.download_rounded,
-              //                     size: 20,
-              //                     color: Colors.white,
-              //                   ),
-              //                 ),
-              //                 const Padding(
-              //                   padding: EdgeInsets.symmetric(vertical: 10.0),
-              //                   child: Text(
-              //                     'DOWNLOAD',
-              //                     style: TextStyle(
-              //                       fontWeight: FontWeight.w500,
-              //                       fontSize: 12,
-              //                       color: Colors.white,
-              //                     ),
-              //                   ),
-              //                 ),
-              //               ],
-              //             ),
-              //           ),
-              //         ],
-              //       ),
-              //     ),
-              //   ],
-              // ),
-              // Column(
-              //   crossAxisAlignment: CrossAxisAlignment.start,
-              //   mainAxisAlignment: MainAxisAlignment.start,
-              //   children: [
-              //     Column(
-              //       crossAxisAlignment: CrossAxisAlignment.start,
-              //       mainAxisAlignment: MainAxisAlignment.start,
-              //       children: [
-              //         const Padding(
-              //           padding: EdgeInsets.symmetric(
-              //             vertical: 15.0,
-              //             horizontal: 10.0,
-              //           ),
-              //           child: Text(
-              //             'PURCHASE MOVIES',
-              //             style: TextStyle(
-              //               fontWeight: FontWeight.bold,
-              //               fontSize: 20,
-              //               color: Colors.red,
-              //             ),
-              //           ),
-              //         ),
-              //         Padding(
-              //           padding: const EdgeInsets.symmetric(
-              //             horizontal: 10.0,
-              //           ),
-              //           child: SizedBox(
-              //             height: 200,
-              //             child: ListView.builder(
-              //               itemCount: movieList.length,
-              //               shrinkWrap: true,
-              //               scrollDirection: Axis.horizontal,
-              //               itemBuilder: (context, index) {
-              //                 return Card(
-              //                   elevation: 5,
-              //                   semanticContainer: true,
-              //                   margin: const EdgeInsets.symmetric(
-              //                       horizontal: 10.0),
-              //                   clipBehavior: Clip.antiAliasWithSaveLayer,
-              //                   shape: RoundedRectangleBorder(
-              //                     borderRadius: BorderRadius.circular(15.0),
-              //                   ),
-              //                   child: Image.asset(
-              //                     'assets/images/${movieList[index]}',
-              //                     fit: BoxFit.cover,
-              //                   ),
-              //                 );
-              //               },
-              //             ),
-              //           ),
-              //         ),
-              //       ],
-              //     ),
-              //   ],
-              // ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  showDialogBox() {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            child: AlertDialog(
+              backgroundColor: Colors.transparent.withOpacity(0.2),
+              title: Text("My Box"),
+              actions: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
